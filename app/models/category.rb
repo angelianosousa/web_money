1e039fa1abd07a7055903c8fc5361f11e0552839
Angeliano <angeliano.sousa@gmail.com>
2022-03-08 19:57:31 -0300

    WIP

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,19 +4,19 @@ class Category < ApplicationRecord
   validates :title, :badge, uniqueness: true
 
   scope :recipes_sumatory, ->(user_profile){ 
-    first.recurrences.where(user_profile: user_profile).includes(:transactions).group(:title).sum(:price_cents) 
+    Category.first.recurrences.where(user_profile: user_profile).includes(:transactions).group(:title).sum(:price_cents) 
   }
 
   scope :expenses_sumatory, ->(user_profile){ 
-    second.recurrences.where(user_profile: user_profile).includes(:transactions).group(:title).sum(:price_cents) 
+    Category.second.recurrences.where(user_profile: user_profile).includes(:transactions).group(:title).sum(:price_cents) 
   }
 
   scope :all_recipes, -> (user_profile){ 
-    first.recurrences.where(user_profile: user_profile).includes(:transactions).sum(:price_cents)
+    Category.first.recurrences.where(user_profile: user_profile).includes(:transactions).sum(:price_cents)
   }
 
   scope :all_expenses, -> (user_profile){ 
-    second.recurrences.where(user_profile: user_profile).includes(:transactions).sum(:price_cents)
+    Category.second.recurrences.where(user_profile: user_profile).includes(:transactions).sum(:price_cents)
   }
 
   scope :balance, ->(recipes, expenses){
