1e3af1cfa30659f7a5a4eed15aac550b542bf16e
Angeliano <angeliano.sousa@gmail.com>
2022-03-10 00:29:42 -0300

    WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,4 @@
 //= require turbolinks
 //= require chartkick
 //= require Chart.bundle
-//= require jquery/dist/jquery.min
-//= require bootstrap/dist/js/bootstrap.bundle.min
-//= require bootstrap-notify/bootstrap-notify.min
-//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl.min
 //= require_tree .
--- app/assets/javascripts/user_devise.coffee
+++ app/assets/javascripts/user_devise.coffee
@@ -1,1 +1,5 @@
+//= require jquery/dist/jquery.min
+//= require bootstrap/dist/js/bootstrap.bundle.min
+//= require bootstrap-notify/bootstrap-notify.min
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl.min
 //= require custom
