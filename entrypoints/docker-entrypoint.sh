02643acd9f1bd6f070823be4aac51a277d3b15bf
Angeliano Sousa <angeliano.sousa@gmail.com>
2023-03-09 00:12:45 -0300

    WIP

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -6,6 +6,6 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-rails db:migrate db:seed
+rails db:create db:migrate db:seed
 RAILS_ENV=production rails assets:precompile
 rails s -b 0.0.0.0
