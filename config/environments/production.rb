b431041b3ee2a8c3674388eb9ca6dabf6d3ec172
Angeliano Sousa <angeliano.sousa@gmail.com>
2023-07-03 00:19:26 -0300

    fixing the problem with rails commands

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts = ENV.fetch('SITE_DOMAIN') { 'webmoney-production.up.railway.app' }
   # Code is not reloaded between requests.
   config.cache_classes = false
 
--- config/initializers/host_config.rb
+++
@@ -1,3 +0,0 @@
-if Rails.env.production?
-  Rails.application.config.hosts = ENV.fetch('SITE_DOMAIN') { 'localhost' }
-end
