b2df7b99da029740fef28d58225c26906cff2916
Vitor-Novaes <vitornovaes27@gmail.com>
2023-10-02 21:04:00 -0300

    fix: Some suit tests not implemented yet

--- spec/views/budgets/index.html.erb_spec.rb
+++ spec/views/budgets/index.html.erb_spec.rb
@@ -3,24 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe 'budgets/index', type: :view do
+  let(:user_profile) { create(:user_profile) }
+
   before(:each) do
+    sign_in(user_profile.user)
+
     assign(:budgets, [
              Budget.create!(
-               objective_name: 'Objective Name',
-               goals_price: '9.99',
-               user_profile: nil
+               objective_name: Faker::Name.unique.name,
+               goals_price: 9.99,
+               user_profile: user_profile
              ),
              Budget.create!(
-               objective_name: 'Objective Name',
-               goals_price: '9.99',
-               user_profile: nil
+               objective_name: Faker::Name.unique.name,
+               goals_price: 9.99,
+               user_profile: user_profile
              )
            ])
   end
 
   it 'renders a list of budgets' do
     render
-    assert_select 'tr>td', text: 'Objective Name'.to_s, count: 2
     assert_select 'tr>td', text: '9.99'.to_s, count: 2
     assert_select 'tr>td', text: nil.to_s, count: 2
   end
