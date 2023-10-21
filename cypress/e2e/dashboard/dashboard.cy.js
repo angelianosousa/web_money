2d24c002faa8d41a76ceea88df94c3616ea792fb
Davi Silva <davi.silva@usabit.com.br>
2023-10-21 15:52:59 -0300

    feat: added tests to show and hide all modal components

--- cypress/e2e/dashboard/dashboard.cy.js
+++ cypress/e2e/dashboard/dashboard.cy.js
@@ -12,4 +12,70 @@ describe("Verificação dos componentes na página de Dashboard ", () => {
     cy.loginSuccessfully();
     cy.navBarTesting(URL);
   });
+
+  it("Deve se o título da página redireciona normalmente.", () => {
+    cy.loginSuccessfully();
+
+    cy.get('a[href="dashboard"]').click();
+
+    cy.url().should("eq", "http://localhost:3000/dashboard");
+  });
+
+  it("Deve testar a abertura dos modais de Receita e Despesa.", () => {
+    cy.loginSuccessfully();
+
+    cy.get('a[data-target="#newRecipeModal"]').click();
+
+    cy.get("#newRecipeModal").should("have.css", "display", "block");
+
+    cy.visit(URL);
+
+    cy.get('a[data-target="#newExpenseModal"]').click();
+
+    cy.get("#newExpenseModal").should("have.css", "display", "block");
+  });
+
+  it("Deve testar a abertura dos modais de Nova conta e Nova recorrência.", () => {
+    cy.loginSuccessfully();
+
+    cy.get("#dropdownActionButton").click();
+
+    cy.get("#dropdownActionButton").should(
+      "have.attr",
+      "aria-expanded",
+      "true"
+    );
+
+    cy.get('a[data-target="#newAccountModal"][data-toggle="modal"]').click();
+
+    cy.wait(1000);
+
+    cy.get("#newAccountModal").find("button.close").click();
+
+    cy.get("#dropdownActionButton").click();
+
+    cy.get("#dropdownActionButton").should(
+      "have.attr",
+      "aria-expanded",
+      "true"
+    );
+
+    cy.get('a[data-target="#newBillModal"][data-toggle="modal"]').click();
+
+    cy.wait(1000);
+
+    cy.get("#newBillModal").find("button.close").click();
+  });
+
+  it("Deve testar a abertura e fechamento do filtro de pesquisa em Dashboard.", () => {
+    cy.loginSuccessfully();
+
+    cy.get('a[data-toggle="collapse"][data-target="#collapseSearch"]').click();
+
+    cy.get("#collapseSearch").should("have.class", "collapse show");
+
+    cy.get('a[data-toggle="collapse"][data-target="#collapseSearch"]').click();
+
+    cy.get("#collapseSearch").should("have.class", "collapse");
+  });
 });
